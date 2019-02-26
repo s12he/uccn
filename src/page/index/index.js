130a6f88aa744333b7c907a107bdda4231f0bda3
S12he <412191389@qq.com>
2019-02-26 15:56:39 +0800

    集团新闻页面添加内容,新增页码器

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -20,25 +20,7 @@ if ($('.ydNav').hasClass('none')) {
       returnDefault:true  //返回默认状态
   });
 }
-var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    slidesPerView: 1,
-    spaceBetween: 0,
-    mousewheel: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    on:{
-      init: function(){
-        swiperAnimateCache(this); //隐藏动画元素 
-        swiperAnimate(this); //初始化完成开始动画
-      }, 
-      slideChangeTransitionEnd: function(){ 
-        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
-      } 
-    }
-});
+
 
 // // 跳转顶部
 $('.top').click(function() {
--- src/page/news/news.js
+++ src/page/news/news.js
@@ -5,4 +5,55 @@ jQuery(".content").slide({
 	mainCell: '.trendsMessage'
 });
 
+//职位介绍详情内容分页
+var index = [];
+for (var i = 0; i < $('.trendsItem').length; i++) {
+	var num = 0;
+	index.push(num)
+	paging($('.trendsItem:eq('+i+')'), index[i], i)
+}
+
+function paging(name, num, i) {
+	//隐藏所有jobUl,以及删除分页器高亮状态
+	function hideRemove(page) {
+		for (var j = 0; j < page.length; j++) {
+			name.find('.page:eq('+j+')').hide();
+			name.find('.num:eq('+j+')').removeClass('active');
+		}
+	}
+	//显示当前page,以及当前分页器页码
+	function showAdd(num) {
+		name.find('.page:eq('+num+')').show();
+		name.find('.num:eq('+num+')').addClass('active')
+	}
+	//点击下一页
+	name.find('.next1').click(function() {
+		var page = name.find('.page')
+		num++;
+		if (num < page.length) {
+			hideRemove(page);
+			showAdd(num);
+		}else {
+			num = page.length - 1
+		}
+	})
+	//点击上一页
+	name.find('.prev1').click(function() {
+		var page = name.find('.page')
+		num--;
+		if (num >= 0) {
+			hideRemove(page);
+			showAdd(num);
+		}else{
+			num = 0;
+		}
+	})
+	//点击页码 
+	name.find('.num').click(function() {
+		hideRemove(name.find('.page'));
+		showAdd(this.innerHTML - 1);
+		num = this.innerHTML - 1;
+	})
+}
+
 Crumbs(2)
